24a1e04ce10bd7072404632af24982d4b4a15a78
PiyushIngale007 <ingalepiyush@gmail>
2021-03-04 14:44:04 +0530

    Questions added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from '../node_modules/axios';
 
 import './App.css';
 import Quiz from './Quiz/Quiz';
 
 class App extends Component {
+  state = {
+    Questions: [],
+    QuestionNumber: 0,
+  };
+  count = 0;
+  flag = false;
+  async getData() {
+    await axios
+      .get('https://limitless-scrubland-76769.herokuapp.com/')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          Questions: response.data.data,
+        });
+      });
+    this.flag = true;
+  }
+  QuestionHandler = () => {
+    if (this.count < 9) {
+      this.setState({
+        QuestionNumber: (this.count += 1),
+      });
+    }
+  };
   render() {
+    if (this.flag === false) {
+      this.getData();
+    }
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
         </header>
-        <Quiz />
+        {this.flag ? <Quiz Ques={this.state} /> : null}
+        <button onClick={this.QuestionHandler}>next</button>
       </div>
     );
   }
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -3,35 +3,54 @@ import './Quiz.css';
 import { InputGroup, FormControl } from 'react-bootstrap';
 
 const Quiz = (props) => {
+  let Q = props.Ques.QuestionNumber;
   return (
     <div className='quiz'>
       <div className='question'>
-        <p>Question : </p>
+        <p>
+          Question {Q + 1}:{props.Ques.Questions[Q].col_2}{' '}
+        </p>
       </div>
       <div className='options'>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Radio name='options' />
+            <InputGroup.Radio name={'options'} />
           </InputGroup.Prepend>
-          <FormControl type='text' value={props.a} readOnly />
+          <FormControl
+            type='text'
+            value={props.Ques.Questions[Q].col_3}
+            readOnly
+          />
         </InputGroup>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Radio name='options' />
+            <InputGroup.Radio name={'options'} />
           </InputGroup.Prepend>
-          <FormControl type='text' readOnly />
+          <FormControl
+            type='text'
+            value={props.Ques.Questions[Q].col_4}
+            readOnly
+          />
         </InputGroup>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Radio name='options' />
+            <InputGroup.Radio name={'options'} />
           </InputGroup.Prepend>
-          <FormControl type='text' readOnly />
+          <FormControl
+            type='text'
+            value={props.Ques.Questions[Q].col_5}
+            readOnly
+          />
         </InputGroup>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Radio name='options' />
+            <InputGroup.Radio name={'options'} />
           </InputGroup.Prepend>
-          <FormControl type='text' readOnly />
+          <FormControl
+            type='text'
+            value={props.Ques.Questions[Q].col_6}
+            readOnly
+          />
         </InputGroup>
       </div>
     </div>
